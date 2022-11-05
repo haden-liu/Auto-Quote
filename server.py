6969adbba6be8be7cefccc3709aa6c7ad29b891e
Haden Liu <haden.liui30@gmail.com>
2022-11-05 12:01:06 +1100

    extract route

--- server.py
+++ server.py
@@ -21,28 +21,38 @@ def calcuation_result():
     width = request.form.get('width')
     height = request.form.get('height')
     weight = request.form.get('weight')
+    amount = request.form.get('amount')
 
-    CBM = int(length) * int(width) * int(height) / 1000000
-    print(type(CBM))
+    volume_unit = int(length) * int(width) * int(height) / 1000000
+    total_volume = volume_unit * int(amount)
+    total_weight = float(weight) * int(amount)
 
-    print(loading_port)
+    print(total_volume)
+    print(total_weight)
+
    
-
     conn = psycopg2.connect('dbname = freight')
     cur = conn.cursor()
 
-    cur.execute('select freight_rate_min, freight_rate_unit, freight_rate_weight from rates where loading_port = %s and discharging_port = %s', [loading_port,discharge_port])
+    cur.execute('select carrier, freight_rate_min, freight_rate_unit, freight_rate_weight from rates where loading_port = %s and discharging_port = %s', [loading_port,discharge_port])
     result = cur.fetchone()
     print(result)
 
-    freight_rate_min, freight_rate_unit, freight_rate_weight = result
+    carrier, freight_rate_min, freight_rate_unit, freight_rate_weight = result
 
-    total = freight_rate_unit * CBM
-    print(total)
+    if freight_rate_min > max(total_volume * freight_rate_unit, total_weight / 1000 * freight_rate_unit):
+        total_rate = freight_rate_min
+    else:
+        total_rate = max(total_volume * freight_rate_unit, total_weight / 1000 * freight_rate_unit)
+
+    print(total_rate)
+
+
+ 
 
 
 
-    return render_template('result.html')
+    return render_template('result.html', amount = amount, CBM = total_volume, weight = total_weight, loading_port = loading_port, discharging_port = discharge_port, carrier = carrier, total = total_rate)
 
 
 app.run(debug=True)
