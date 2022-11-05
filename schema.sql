7141ef5d1a76160d747b615eff57ffae8cfb989f
Haden Liu <haden.liui30@gmail.com>
2022-11-05 17:47:05 +1100

    Saturday work

--- schema.sql
+++ schema.sql
@@ -1,17 +1,18 @@
 -- drop table if exists coloaders;
--- drop table if exists rates;
+drop table if exists rates;
 
--- create table rates (
---     id serial primary key,
---     carrier varchar(50) not null,
---     freight_rate_min float(1) not null,
---     freight_rate_unit float(1) not null,
---     fuel_rate float(1),
---     loading_port varchar(50) not null,
---     discharging_port varchar(50) not null,
---     valid_date date not null,
---     coloader_ID integer
--- )
+create table rates (
+    id serial primary key,
+    carrier varchar(50),
+    freight_rate_min float(1),
+    freight_rate_unit float(1),
+    fuel_rate float(1),
+    loading_port varchar(50),
+    discharging_port varchar(50),
+    valid_date date
+
+)
 
 -- alter table rates drop column coloader_ID
-alter table rates add column freight_rate_weight float(1) not null
+-- alter table rates add column freight_rate_weight float(1) not null
+-- alter table rates drop column freight_rate_weight
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import psycopg2
+import pdfkit
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,7 +24,7 @@ def calcuation_result():
     weight = request.form.get('weight')
     amount = request.form.get('amount')
 
-    volume_unit = int(length) * int(width) * int(height) / 1000000
+    volume_unit = float(length) * float(width )* float(height) / 1000000
     total_volume = volume_unit * int(amount)
     total_weight = float(weight) * int(amount)
 
@@ -34,25 +35,47 @@ def calcuation_result():
     conn = psycopg2.connect('dbname = freight')
     cur = conn.cursor()
 
-    cur.execute('select carrier, freight_rate_min, freight_rate_unit, freight_rate_weight from rates where loading_port = %s and discharging_port = %s', [loading_port,discharge_port])
+    cur.execute('select carrier, freight_rate_min, freight_rate_unit, fuel_rate, valid_date from rates where loading_port = %s and discharging_port = %s', [loading_port,discharge_port])
     result = cur.fetchone()
     print(result)
 
-    carrier, freight_rate_min, freight_rate_unit, freight_rate_weight = result
+    carrier, freight_rate_min, freight_rate_unit, fuel_rate, valid_date = result
 
-    if freight_rate_min > max(total_volume * freight_rate_unit, total_weight / 1000 * freight_rate_unit):
+    if freight_rate_min > max(total_volume * 1000 / 6 * freight_rate_unit, total_weight  * freight_rate_unit):
         total_rate = freight_rate_min
     else:
-        total_rate = max(total_volume * freight_rate_unit, total_weight / 1000 * freight_rate_unit)
+        total_rate = max(total_volume * 1000 / 6 * freight_rate_unit, total_weight * freight_rate_unit)
 
     print(total_rate)
+    print(valid_date)
+    total_fuel = max(total_volume * 1000 / 6 * fuel_rate, total_weight * fuel_rate)
 
 
- 
+    return render_template('result.html', amount = amount, CBM = round(total_volume, 2), weight = round(total_weight,2), loading_port = loading_port, discharging_port = discharge_port, carrier = carrier, total = round(total_rate, 2), total_fuel = round(total_fuel, 2), valid_date = valid_date)
+
+@app.route('/addrate', methods=['POST', 'GET'])
+def addrate():
+    loading_port = request.form.get('loadingport')
+    discharging_port = request.form.get('dischargingport')
+    carrier = request.form.get('carrier')
+    minimum_rate = request.form.get('minimum_rate')
+    unit_rate = request.form.get("unit_rate")
+    fuel_rate = request.form.get('fuel_rate')
+    valid_date = request.form.get('valid_date')
+
+    print(discharging_port)
 
 
+    conn = psycopg2.connect('dbname = freight')
+    cur =conn.cursor()
 
-    return render_template('result.html', amount = amount, CBM = total_volume, weight = total_weight, loading_port = loading_port, discharging_port = discharge_port, carrier = carrier, total = total_rate)
+    cur.execute('INSERT INTO rates (carrier,freight_rate_min,freight_rate_unit,fuel_rate,loading_port,discharging_port,valid_date) values (%s,%s,%s,%s,%s,%s,%s)', [carrier, minimum_rate,unit_rate,fuel_rate,loading_port,discharging_port,valid_date])
 
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    return render_template('addrate.html')
 
 app.run(debug=True)
